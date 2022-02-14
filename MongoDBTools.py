7c99b9ecc970a01f69a489bd59858c6f8eb0314f
Jack Hobbs <72261512+Thehobbses@users.noreply.github.com>
2022-02-13 20:46:57 -0800

    Completely refactored to remove need for user input, now aiming to build code that can be run via Airflow

--- MongoDBTools.py
+++ MongoDBTools.py
@@ -3,6 +3,7 @@ from time import time
 import requests
 import multiprocessing as mp
 import pymongo as pm
+from pymongo import database
 from pymongo.errors import ConnectionFailure
 from typing import List, Dict, AnyStr, Set
 
@@ -33,77 +34,6 @@ def parallelized_request(processor_count: int, link_list: List) -> List:
     return results
 
 
-def get_input(prompt: AnyStr, blacklist: bool, restricted: List, intgr: bool, return_bool: bool):
-    """
-    simplifies display of input requests, and captures user response while handling restrictions
-    :param prompt: string to prompt user input
-    :param blacklist: changes the character restriction type (True = blacklist, False = whitelist)
-    :param restricted: list of restricted characters
-    :param intgr: logic for if the user input should be returned as int or str (True = int)
-    :param return_bool: if true, the first value in restricted list is considered True and the others False (y/n)
-    :return: user input value
-    """
-    user_input = input(prompt)
-
-    if intgr:
-        user_input = int(user_input)
-
-    if blacklist:
-        while any([char in restricted for char in user_input]):
-            print(f'Please do not use restricted characters: {restricted}')
-            user_input = input(prompt)
-            if intgr:
-                user_input = int(user_input)
-
-    else:
-        while user_input not in restricted:
-            print(f'Please only use the following characters: {restricted}')
-            user_input = input(prompt)
-            if intgr:
-                user_input = int(user_input)
-
-    if return_bool:
-        if user_input == restricted[0]:
-            return True
-        else:
-            return False
-
-    else:
-        return user_input
-
-
-def select_pymongo_object(pm_obj_list: list, operation_desc: AnyStr, pm_obj_desc: AnyStr):
-    """
-    accepts user input to select the desired MongoDB object to read and write from
-    :param pm_obj_list: list of MongoDB objects, generally from <client>.list_all_databases or <db>.list_all_collections
-    :param operation_desc: description of operation
-    :param pm_obj_desc: short string description of object type in pm_obj_list
-    :return: string corresponding to a object in the list based on user provided index
-    """
-    # pull in list of objects and print their list index and string name
-    index = -1
-    string_list = f'\n{operation_desc}:\n'
-    for obj in pm_obj_list:     # append index: object for each in list
-        index += 1
-        string_list += f'{index}: {obj}\n'
-
-    # if the string is empty, print None
-    if string_list == '':
-        print(f'No {pm_obj_desc} found. Initiate new {pm_obj_desc} then try again.\n')
-
-    else:
-        try:
-            print(string_list)
-            # get user to select from list of available objects, or make new object
-            select_prompt = f'Input index number from list above: '
-            user_input = get_input(prompt=select_prompt, blacklist=False,
-                                   restricted=[num for num in range(len(pm_obj_list))],
-                                   intgr=True, return_bool=False)
-            return pm_obj_list[user_input]
-        except ValueError:  # probably just a little quick on the trigger and passed nothing
-            select_pymongo_object(pm_obj_list, pm_obj_desc)
-
-
 def pymongo_local_client(host: AnyStr, port: int) -> pm.MongoClient:
     """
     Establish a client with a specified name, host, and port
@@ -129,147 +59,81 @@ def pymongo_client(link: AnyStr) -> pm.MongoClient:
     return client
 
 
-def grab_db(client: pm.MongoClient, database: AnyStr) -> pm.database.Database:
+def is_restricted(user_input: AnyStr):
+    """
+    tests if the given string contains Mongo restricted characters
+    :param user_input: string value from the database dictionary
+    :return: raises error if containing restricted
+    """
+    restricted_char = [r'/', r'\\', r'.', r'"', '$', '*', '<', '>', ':', '|', '?']  # Mongo restricted characters
+    if any([char in restricted_char for char in user_input]):
+        raise ValueError(f'User Input: {user_input} includes Mongo restricted characters ({restricted_char})')
+
+
+def initiate_database(client: pm.MongoClient, database_str: AnyStr) -> pm.database:
     """
     Grabs a database from the specified client
     :param client: Mongo client connection
-    :param database: Mongo database string name
+    :param database_str: Mongo database string name
     :return: PyMongo database connection
     """
-    db = client[database]
-    return db
+    is_restricted(database_str)
+    database_obj = client[database_str]
+    return database_obj
 
 
-def grab_db_list(client: pm.MongoClient) -> List:
-    """
-    Lists all databases for the supplied client
-    :param client: Mongo client connection
-    :return: list of database names
-    """
-    database_list = client.list_database_names()
-    return database_list
-
-
-def drop_db(client: pm.MongoClient, database: AnyStr) -> pm.database.Database:
-    """
-    Drop a database from the specified client
-    :param client: Mongo client connection
-    :param database: Mongo database string name
-    :return: None
-    """
-    client[database].drop()
-    print(f'Dropped database: {database}')
-
-
-def grab_collection(client: pm.MongoClient, database: AnyStr, collection: AnyStr) -> pm.collection:
+def initiate_collection(database_obj: pm.database, collection: AnyStr) -> pm.collection:
     """
     Grabs a database collection from the specified client
-    :param client: Mongo client connection
-    :param database: Mongo database string name
+    :param database_obj: Mongo database connection
     :param collection: Mongo collection string name
     :return: a PyMongo collection
     """
-    db = client[database]
-    col = db[collection]
-    return col
+    is_restricted(collection)
+    collection_obj = database_obj[collection]
+    return collection_obj
 
 
-def grab_collection_list(client: pm.MongoClient, database: AnyStr) -> List:
+def grab_collection_fields(collection: pm.collection, filter_bool: bool, object_key: Dict) -> List:
     """
-    Lists all collections in a database
-    :param client: Mongo client connection
-    :param database: Mongo database string name
-    :return: a list of collection names, filter removes system collections
-    """
-    filter_sys = {"name": {"$regex": r"^(?!system\.)"}}     # Drops all system objects
-    collection_dict_list = client[database].list_collections(filter=filter_sys)
-    collection_list = list()
-    for each in collection_dict_list:
-        collection_list.append(each['name'])
-    return collection_list
-
-
-def grab_collection_object_values(client: pm.MongoClient, database: AnyStr, collection: AnyStr,
-                                  filter_bool: bool, object_key: dict) -> list:
-    """
-    Grabs a set of specified fields from the specified collection
+    Grabs a set of specified fields from a collection; result is received as a pymongo cursor
     For more detail on structuring object_key: https://docs.mongodb.com/manual/reference/method/db.collection.find/
-    :param client: Mongo client connection
-    :param database: Mongo database string name
-    :param collection: Mongo collection string name
+    :param collection: Mongo collection connection
     :param filter_bool: return selected fields for all objects (F), or objects with field values matching object_key (T)
     :param object_key: a Mongo formatted dict to return fields based on criteria
     :return: a list of values from all objects in the collection
     """
-    db = client[database]
-    col = db[collection]
+    return_list = List
+
     if filter_bool:
-        result = col.find(object_key)
+        result = collection.find(object_key)
     else:
-        result = col.find({}, object_key)
-    return result
+        result = collection.find({}, object_key)
+
+    for record in result:
+        return_list.append(record.values())  # append values to list for requests
+
+    return return_list
 
 
-def drop_collection(client: pm.MongoClient, database: AnyStr, collection: AnyStr) -> pm.collection:
-    """
-    Drop a database collection from the specified client
-    :param client: Mongo client connection
-    :param database: Mongo database string name
-    :param collection: Mongo collection string name
-    :return: None
-    """
-
-    db = client[database]
-    col = db[collection]
-    col.drop()
-    print(f'Dropped database collection: {collection}')
-
-
-def drop_collection_records(documents, doc_desc: AnyStr, client: pm.MongoClient,
-                            database: AnyStr, collection: AnyStr) -> pm.collection:
-    """
-    Drop a collection record from the specified client
-    :param documents: list of documents in collection, or single dict
-    :param doc_desc: description of documents dropped
-    :param client: Mongo client connection
-    :param database: Mongo database string name
-    :param collection: Mongo collection string name
-    :return: None
-    """
-    db = client[database]
-    col = db[collection]
-    col.delete_many(documents)
-    print(f'Dropped {doc_desc} from collection: {collection}')
-
-
-def post_documents(documents: List, client: pm.MongoClient,
-                   database: AnyStr, collection: AnyStr, uri: AnyStr) -> List:
+def post_documents(documents: List, collection: pm.collection) -> List:
     """
     posts documents to a specified database
-    :param client: Mongo client connection
-    :param database: Mongo database string name
-    :param collection: Mongo collection string name
+    :param collection: Mongo collection object
     :param documents: list of document dictionaries to post to the database
-    :param uri: connection link url for error handling
     :return: list of uploaded object ids
     """
     start_time = time()
-    print(f'Posting {len(documents)} documents to {collection}')
-    db = client[database]
-    col = db[collection]
     err_loop = 0
 
     while True:    # sometimes the remote connection fails on large upload and needs to be reset
         try:
-            batch_upload = col.insert_many(documents)
+            batch_upload = collection.insert_many(documents)
 
         except ConnectionResetError:
             err_loop += 1
             print(f'Connection to remote host failed... [Attempt: {err_loop}]')
-            client = pymongo_client(uri)
-            db = client[database]
-            col = db[collection]
-            batch_upload = col.insert_many(documents)
+            batch_upload = collection.insert_many(documents)
 
         print(f'Success: data pushed to MongoDB Atlas in {round(time() - start_time, 2)} seconds')
         return batch_upload.inserted_ids
---
+++ PushToMongoDB.py
@@ -0,0 +1,91 @@
+# MongoDB Exploration
+# Jack Hobbs
+
+# libraries and Distributions
+import os
+import multiprocessing as mp
+from typing import List
+
+# function Modules
+import MongoDBTools
+
+# environmental Vars for DB access and data-pathing
+# as far as I can tell, access token generation is not possible in the free version of Atlas
+# passing user credentials as string variables is not a best practice
+PMCLIENT = list(os.environ['pmclient'].split(","))  # Client to use
+PMDATABASE = os.environ['pmdatabase']  # Database to query
+USER = os.environ['user']  # Mongo username
+PASSWORD = os.environ['pmpassword']  # Password for access to the cloud Mongo server
+uri = f"mongodb+srv://{USER}:{PASSWORD}@jacktestingcluster.z8hzo.mongodb.net/{PMDATABASE}?retryWrites=true&w=majority"
+
+mongo_database_list = [{'Database Name': 'Newspaper',
+                        'index collection': 'newspaper_index',
+                        'detail collections': [{'collection name':'newspaper_details',
+                                                'pymongo index query': {'_id': 0, 'url': 1}}],
+                        'url': 'https://chroniclingamerica.loc.gov/newspapers.json',
+                        'uri': uri,
+                        'objects access key': 'newspapers'}]
+
+# multiprocessing info
+mp_max = mp.cpu_count()     # for the discerning connoisseur of computational cores
+
+
+def input_type_governor(database_list: List):
+    """
+    parses a list of databases and checks that each field is passing correct data types
+    :param database_list: list of dicts containing database and collection parameters
+    :return: pass or fail codes
+    """
+    for database_dict in database_list:
+        print(0)
+
+
+def main(database_list: List):
+    """
+    executes insertion of data to MongoDB
+    :param database_list: list of dicts containing database and collection parameters
+    """
+
+    # establishing the Mongo client
+    pymongo_client = MongoDBTools.pymongo_client(uri)
+
+    for database_dict in database_list:
+        # initialize database and collections from details in dictionary
+        active_database = MongoDBTools.initiate_database(
+            client=pymongo_client,
+            database_str=database_dict['Database Name'])
+
+        index_collection = MongoDBTools.initiate_collection(
+            database_obj=active_database,
+            collection=database_dict['index collection'])
+
+        # grab url from database_dict that links to the index JSON
+        index_json_list = MongoDBTools.parallelized_request(mp_max, [database_dict['url']])
+
+        # post all records from the index JSON to the index collection
+        MongoDBTools.post_documents(documents=index_json_list, collection=index_collection)
+
+        # loop through each detail collection and refresh required documents
+        for detail_collection in database_dict['detail collections']:
+            active_detail_collection = MongoDBTools.initiate_collection(
+                database_obj=active_database,
+                collection=detail_collection['name'])
+
+            # grab list of urls to access
+            url_upload_list = MongoDBTools.grab_collection_fields(
+                collection=active_detail_collection,
+                filter_bool=False,
+                object_key=detail_collection['pymongo index query'])
+
+            details_json_list = MongoDBTools.parallelized_request(mp_max, url_upload_list)
+
+            # push details documents to selected collection
+            MongoDBTools.post_documents(documents=details_json_list,
+                                        collection=detail_collection)
+
+    # close client to prevent accidental billing
+    pymongo_client.close()
+
+
+if __name__ == "__main__":
+    main(mongo_database_list)
