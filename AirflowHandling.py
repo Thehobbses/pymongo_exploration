878e476129a3082fe666809fb0aacf451e7d7871
Jack Hobbs <72261512+Thehobbses@users.noreply.github.com>
2022-02-14 19:31:34 -0800

    refactored to GCS for Airflow 2.0

--- AirflowHandling.py
+++ AirflowHandling.py
@@ -4,45 +4,55 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
 
+import datetime
+
+from airflow import models
+from airflow.operators import bash
+
+
 EMAIL = os.environ['email']
 WORKING_DIRECTORY = os.environ['working_dir']
+
+YESTERDAY = datetime.datetime.now() - datetime.timedelta(days=1)
 
 default_args = {
     'owner': 'Jack Hobbs',
     'depends_on_past': False,
-    'start_date': days_ago(0),
     'email': [EMAIL],
     'email_on_failure': True,
+    'email_on_retry': False,
     'retries': 1,
-    'retry_delay': timedelta(minutes=2)
+    'retry_delay': datetime.timedelta(minutes=5),
+    'start_date': YESTERDAY,
 }
 
-# initiate a directed acyclic graph
-dag = DAG(
-    'airflow pipeline',
-    default_args=default_args,
-    description='simple pipeline from the Library of Congress',
-    schedule_interval=timedelta(days=1))
+with models.DAG(
+        'airflow_pipeline',
+        catchup=False,
+        default_args=default_args,
+        description='simple pipeline from the Library of Congress',
+        schedule_interval=datetime.timedelta(days=1)) as dag:
 
-# initiate tasks using py scripts as bash operators
-push_to_mongo = BashOperator(
-    task_id='push_to_mongo',
-    bash_command=f'{WORKING_DIRECTORY}/PushToMongoDB.py',
-    dag=dag)
+    # Print the dag_run id from the Airflow logs
+    push_to_mongo = bash.BashOperator(
+        task_id='push_to_mongo',
+        bash_command=f'{WORKING_DIRECTORY}/PushToMongoDB.py',
+        dag=dag)
 
-pull_from_mongo = BashOperator(
-    task_id='pull_from_mongo',
-    depends_on_past=False,
-    bash_command=f'{WORKING_DIRECTORY}/PullFromMongoDB.py',
-    retries=3,
-    dag=dag)
+    pull_from_mongo = bash.BashOperator(
+        task_id='pull_from_mongo',
+        depends_on_past=False,
+        bash_command=f'{WORKING_DIRECTORY}/PullFromMongoDB.py',
+        retries=3,
+        dag=dag)
 
-visualize_mongo_data = BashOperator(
-    task_id='visualize_mongo_data',
-    depends_on_past=False,
-    bash_command=f'{WORKING_DIRECTORY}/VisualizationHandling.py',
-    retries=3,
-    dag=dag)
+    visualize_mongo_data = bash.BashOperator(
+        task_id='visualize_mongo_data',
+        depends_on_past=False,
+        bash_command=f'{WORKING_DIRECTORY}/VisualizationHandling.py',
+        retries=3,
+        dag=dag)
+
 
 # Ordering the DAG. '>>' initiates task 2 after task 1 is finished
 push_to_mongo >> pull_from_mongo >> visualize_mongo_data
